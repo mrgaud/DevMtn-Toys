f7327e9aefb917a268e2fafb12004feb32452c12
Michael Gaudet <Michael.G224@gmail.com>
2017-03-15 21:09:38 -0500

    added exact change problem

---
+++ exactChange.js
@@ -0,0 +1,9 @@
+// Create a function that takes in two arguments(balance and amount paid) return the smallest amount of bills/coins to the customer as possible.
+// change('19.40','20.00') should return "2 quarters, 1 dime"
+// change('10.00', '10.00') should return "exact change"
+// change('100.00', '90.00') should return "$10 still owed"
+
+function change(bal, paid){
+
+}
+change('100.25')
