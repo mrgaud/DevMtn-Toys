0f26f35e74751ec512f67017acab74bc21484a93
Michael Gaudet <Michael.G224@gmail.com>
2017-03-18 15:09:04 -0500

    solved sqrt problem

--- isSqrt.js
+++ isSqrt.js
@@ -1,3 +1,21 @@
 // create a function that will check if a number is a square root or not
 // isSqrt(144) should return "144 is a square root of 12"
 // isSqrt(133) should return "133 is not a square root"
+function isSqrt(num) {
+    var i = 1,result;
+    result = 'pie'
+    while(true){
+        result = i * i
+        if(result > num){
+            return `${num} is not a squared number`
+        }else if(result === num){
+            return `${num} is a squared number, ${i}*${i}`
+        }
+        else{
+            i++
+        }
+    }
+}
+
+
+console.log(isSqrt(25));
